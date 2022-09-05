b440aef9d6d3f97908782ae8b119c94e27a76a7e
awaisshahid965 <awaisshahid965@gmail.com>
2022-09-05 21:26:59 +0500

    Code Completed...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useId } from 'react';
+
+let explorer = {
+  name: 'root',
+  isFolder: true,
+  items: [
+    {
+      name: 'public',
+      isFolder: true,
+      items: [
+        {
+          name: 'index.html',
+          isFolder: false,
+        },
+        {
+          name: 'manifest.json',
+          isFolder: false,
+        }
+      ]
+    },
+    {
+      name: 'src',
+      isFolder: true,
+      items: [
+        {
+          name: 'assets',
+          isFolder: true,
+          items: [
+            {
+              name: 'banner.jpeg',
+              isFolder: false,
+            }
+          ]
+        },
+        {
+          name: 'index.js',
+          isFolder: false,
+        },
+        {
+          name: 'index.css',
+          isFolder: false,
+        }
+      ]
+    }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='sidebar'>
+        <Folder explorer={explorer} />
+      </div>
+      <div className='main' style={{color: 'crimson',}}>
+        Files Open Here
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+ function Folder(props) {
+
+  let { explorer } = props;
+  
+  const [showChilds, setShowChilds] = useState(explorer.name === 'root' ? true : false);
+
+  return (
+    explorer.isFolder ?
+    <div className='folder'>
+      <div className='parent' onClick={() => { setShowChilds(!showChilds) }}>
+        {explorer.name}
+      </div>
+      {
+        showChilds &&
+        <div className='childs'>
+          {
+            explorer.items.map((item, i) => (
+              <Folder explorer={item} key={`${explorer.name}-${i}`} />
+            ))
+          }
+        </div>
+      }
+    </div>
+    :
+    <div className='parent'>{explorer.name}</div>
+  );
+
+ }
